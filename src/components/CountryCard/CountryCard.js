2a9cbfa53be3c8dc61d735b7b102e0782d079243
Cole Rossman <colerossman99@gmail.com>
2022-03-14 18:04:46 -0700

    CountryCard.js complete

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import './CountryCard.css';
 
-// export default function CountryCard({})
+export default function CountryCard({ name, iso2 }) {
+  return (
+    <div className='country-card'>
+      <h3>{name}</h3>
+      <img alt='name' width="72" height="54" src={`https://flagcdn.com/72x54/${iso2.toLowerCase()}.png`} />
+    </div>
+  );
+}

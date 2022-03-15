b45b5aa824824dadd1eecf97488435233f2fd168
Cole Rossman <colerossman99@gmail.com>
2022-03-14 17:16:42 -0700

    imports on Main.js complete

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CountryCard from '../../components/CountryCard/CountryCard';
+import Filter from '../../components/Controls/Filter';
+import { fetchCountries } from '../../services/countries';
+
+import './Main.css';
 
 export default function Main() {
   return (
